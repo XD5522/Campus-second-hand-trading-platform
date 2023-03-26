c06306cefaa904b4196e51620e33ab3bbed3af6f
尘沉 <79737144+User-C7@users.noreply.github.com>
2023-03-26 23:01:48 +0800

    2

--- src/main/java/com/example/campus_second_hand_trading_platform/CampusSecondHandTradingPlatformApplication.java
+++ src/main/java/com/example/campus_second_hand_trading_platform/CampusSecondHandTradingPlatformApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CampusSecondHandTradingPlatformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CampusSecondHandTradingPlatformApplication.class, args);//2
+        SpringApplication.run(CampusSecondHandTradingPlatformApplication.class, args);//23
     }
 
 
