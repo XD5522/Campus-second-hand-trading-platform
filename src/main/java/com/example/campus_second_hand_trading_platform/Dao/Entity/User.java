f597f58b598148cb0680fcd3b11493ef9311d95e
尘沉 <79737144+User-C7@users.noreply.github.com>
2023-03-26 14:52:13 +0800

    1

---
+++ src/main/java/com/example/campus_second_hand_trading_platform/Dao/Entity/User.java
@@ -0,0 +1,13 @@
+package com.example.campus_second_hand_trading_platform.Dao.Entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/example/campus_second_hand_trading_platform/Dao/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.campus_second_hand_trading_platform.Dao.Repository;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
