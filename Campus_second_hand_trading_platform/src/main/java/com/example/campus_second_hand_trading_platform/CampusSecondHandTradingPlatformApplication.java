dab70bbe6d374369b0424656ef6f6d3b68db3029
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-03-26 13:51:59 +0800

    init

---
+++ Campus_second_hand_trading_platform/src/main/java/com/example/campus_second_hand_trading_platform/CampusSecondHandTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_second_hand_trading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSecondHandTradingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusSecondHandTradingPlatformApplication.class, args);
+    }
+
+}
---
+++ Campus_second_hand_trading_platform/src/test/java/com/example/campus_second_hand_trading_platform/CampusSecondHandTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_second_hand_trading_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSecondHandTradingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
