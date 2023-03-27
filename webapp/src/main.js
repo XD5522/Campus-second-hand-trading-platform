7d05835a97fe9696d53998c22316ccff1aa7cabe
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-03-27 22:36:56 +0800

    加入element plus

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+
 
 createApp(App).use(store).use(router).mount('#app')
+
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
