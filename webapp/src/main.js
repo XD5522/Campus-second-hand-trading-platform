bedbd3de38d455898dcb9e69980931b75f71c1e5
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-03-28 00:28:23 +0800

    修复 lang = ts 的bug

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
 
-app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.use(store).use(ElementPlus).use(router).mount('#app')
+
+
