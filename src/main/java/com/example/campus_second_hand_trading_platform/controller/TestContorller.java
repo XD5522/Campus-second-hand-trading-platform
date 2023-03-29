a078128ef6c59bdb8a7f49304c4be821c1be839c
尘沉 <79737144+User-C7@users.noreply.github.com>
2023-03-29 14:49:20 +0800

    1

--- src/main/java/com/example/campus_second_hand_trading_platform/controller/TestContorller.java
+++ src/main/java/com/example/campus_second_hand_trading_platform/controller/TestContorller.java
@@ -1,9 +1,13 @@
 package com.example.campus_second_hand_trading_platform.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin
 public class TestContorller {
     @GetMapping("/test")
     public String Test(){
