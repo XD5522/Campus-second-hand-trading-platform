f37c8e15182f6920d6b301702f93e1289758cb2d
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-04-02 22:20:15 +0800

    update app.vue

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,39 +1,52 @@
 <template>
-    <el-menu
+    <div class="menu-container">
+        <el-menu
             :default-active="activeIndex"
             class="el-menu-demo"
             mode="horizontal"
             :ellipsis="false"
             @select="handleSelect"
-    >
-        <el-menu-item index="/">LOGO</el-menu-item>
-        <div class="flex-grow"/>
-        <el-menu-item index="/about">首页</el-menu-item>
-        <el-menu-item index="/about">哈哈哈</el-menu-item>
-        <el-menu-item index="/about">哈哈哈哈</el-menu-item>
-        <el-menu-item index="/about">呜呜呜呜</el-menu-item>
-        <div class="flex-grow"/>
-        <el-menu-item index="/about">[头像]</el-menu-item>
+        >
+            <el-menu-item index="/">LOGO</el-menu-item>
+            <div class="flex-grow"/>
+            <el-menu-item index="/about">首页</el-menu-item>
+            <el-menu-item index="/about">哈哈哈</el-menu-item>
+            <el-menu-item index="/about">哈哈哈哈</el-menu-item>
+            <el-menu-item index="/about">呜呜呜呜</el-menu-item>
+            <div class="flex-grow"/>
+<!--            <img src="./assets/Account-picture.png" >-->
 
-        <el-sub-menu index="2">
-            <template #title>用户名</template>
-            <el-menu-item index="2-1">个人中心</el-menu-item>
-            <el-menu-item index="2-2">购物车</el-menu-item>
-            <el-menu-item index="2-3">注销</el-menu-item>
-        </el-sub-menu>
-    </el-menu>
+            <el-sub-menu index="2">
+                <template #title>用户名</template>
+                <el-menu-item index="2-1">个人中心</el-menu-item>
+                <el-menu-item index="2-2">购物车</el-menu-item>
+                <el-menu-item index="2-3">注销</el-menu-item>
+            </el-sub-menu>
+        </el-menu>
+    </div>
+
 
 
     <router-view/>
 </template>
 
 <style>
-
-
-img {
-    max-width: 100%;
-    max-height: 100%;
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
+
+
+.menu-container{
+    height: 80px;
+}
+el-menu{
+    height: 100%;
+}
+
 
 .flex-grow {
     flex-grow: 1;
