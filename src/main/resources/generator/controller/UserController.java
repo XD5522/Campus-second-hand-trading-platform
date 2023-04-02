3ae691b75b5e0341f4bb4590ac9245a3ad9ac45d
尘沉 <79737144+User-C7@users.noreply.github.com>
2023-04-02 23:08:27 +0800

    mybatis-plus-generator

---
+++ src/main/resources/generator/controller/UserController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author cc
+ * @since 2023-04-02
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
