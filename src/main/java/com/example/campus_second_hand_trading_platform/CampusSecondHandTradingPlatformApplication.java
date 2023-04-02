cb0eb97ba3b89ae6708caf3cfb6571dbb8f7c165
尘沉 <79737144+User-C7@users.noreply.github.com>
2023-04-02 15:19:46 +0800

    mybatits-plus

--- src/main/java/com/example/campus_second_hand_trading_platform/CampusSecondHandTradingPlatformApplication.java
+++ src/main/java/com/example/campus_second_hand_trading_platform/CampusSecondHandTradingPlatformApplication.java
@@ -1,9 +1,11 @@
 package com.example.campus_second_hand_trading_platform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class CampusSecondHandTradingPlatformApplication {
 
     public static void main(String[] args) {
