6f6efa459a90c709c61269e2630a16dafb94881b
尘沉 <79737144+User-C7@users.noreply.github.com>
2023-04-03 23:26:33 +0800

    delete generator plugin

---
+++ src/main/resources/generator/entity/User.java
@@ -0,0 +1,49 @@
+package entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author cc
+ * @since 2023-04-03
+ */
+@TableName("t_user")
+@ApiModel(value = "User对象", description = "")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+        "id = " + id +
+        ", name = " + name +
+        "}";
+    }
+}
---
+++ src/main/resources/generator/service/IUserService.java
@@ -0,0 +1,16 @@
+package service;
+
+import entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author cc
+ * @since 2023-04-03
+ */
+public interface IUserService extends IService<User> {
+
+}
