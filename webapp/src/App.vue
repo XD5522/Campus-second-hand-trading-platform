9f28b43416b7a882fb6210c9e47193992066abd3
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-04-17 22:34:34 +0800

    update

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,29 +1,6 @@
 <template>
     <div class="menu-container">
         <el-header v-if="showNavbar">
-            <el-menu
-                    :default-active="activeIndex"
-                    class="el-menu-demo"
-                    mode="horizontal"
-                    :ellipsis="false"
-                    @select="handleSelect"
-            >
-                <el-menu-item index="/">LOGO</el-menu-item>
-                <div class="flex-grow"/>
-                <el-menu-item index="/admin">管理员</el-menu-item>
-                <el-menu-item index="/about">哈哈哈</el-menu-item>
-                <el-menu-item index="/about">哈哈哈哈</el-menu-item>
-                <el-menu-item index="/about">呜呜呜呜</el-menu-item>
-                <div class="flex-grow"/>
-                <!--            <img src="./assets/Account-picture.png" >-->
-
-                <el-sub-menu index="2">
-                    <template #title>用户名</template>
-                    <el-menu-item index="2-1">个人中心</el-menu-item>
-                    <el-menu-item index="2-2">购物车</el-menu-item>
-                    <el-menu-item index="2-3">注销</el-menu-item>
-                </el-sub-menu>
-            </el-menu>
             <navbar></navbar>
         </el-header>
     </div>
