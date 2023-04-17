c42c6326387ce50ab95a64ad8e83f4b96383b10d
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-04-17 23:11:25 +0800

    搭建管理员登陆界面

--- webapp/src/views/AdminLogin.vue
+++ webapp/src/views/AdminLogin.vue
@@ -1,100 +1,92 @@
 <template>
-
+    <div class="login-container">
+        <el-card class="login-card">
+            <div class="login-form">
+                <h2 class="login-title">用户登录</h2>
+                <el-form ref="loginForm" :model="form" :rules="rules" label-width="0px">
+                    <el-form-item prop="username">
+                        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="handleLogin">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </el-card>
+    </div>
 </template>
-<script lang="ts">
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ElForm, ElFormItem, ElInput, ElButton, ElCard } from 'element-plus';
+
+interface Form {
+    username: string;
+    password: string;
+}
+
+export default defineComponent({
+    name: 'Login',
+    components: {
+        ElForm,
+        ElFormItem,
+        ElInput,
+        ElButton,
+        ElCard
+    },
+    data() {
+        return {
+            form: {
+                username: '',
+                password: ''
+            } as Form,
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' }
+                ]
+            }
+        };
+    },
+    methods: {
+        handleLogin() {
+            (this.$refs.loginForm as any).validate(valid => {
+                if (valid) {
+                    // 发送登录请求
+                    console.log('登录成功');
+                } else {
+                    console.log('登录失败');
+                    return false;
+                }
+            });
+        }
+    }
+});
 </script>
 
-<!--<template>-->
-<!--    <div class="login-container">-->
-<!--        <el-card class="login-card">-->
-<!--            <div class="login-form">-->
-<!--                <h2 class="login-title">用户登录</h2>-->
-<!--                <el-form ref="loginForm" :model="form" :rules="rules" label-width="0px">-->
-<!--                    <el-form-item prop="username">-->
-<!--                        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item prop="password">-->
-<!--                        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>-->
-<!--                    </el-form-item>-->
-<!--                    <el-form-item>-->
-<!--                        <el-button type="primary" @click="">登录</el-button>-->
-<!--                    </el-form-item>-->
-<!--                </el-form>-->
-<!--            </div>-->
-<!--        </el-card>-->
-<!--    </div>-->
-<!--</template>-->
+<style scoped>
+.login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
 
-<!--<script lang="ts">-->
-<!--import { defineComponent } from 'vue';-->
-<!--import { ElForm, ElFormItem, ElInput, ElButton, ElCard } from 'element-plus';-->
-<!--import 'element-plus/lib/theme-chalk/index.css';-->
+.login-card {
+    width: 360px;
+}
 
-<!--interface Form {-->
-<!--    username: string;-->
-<!--    password: string;-->
-<!--}-->
+.login-form {
+    padding: 20px;
+}
 
-<!--export default defineComponent({-->
-<!--    name: 'Login',-->
-<!--    components: {-->
-<!--        ElForm,-->
-<!--        ElFormItem,-->
-<!--        ElInput,-->
-<!--        ElButton,-->
-<!--        ElCard-->
-<!--    },-->
-<!--    data() {-->
-<!--        return {-->
-<!--            form: {-->
-<!--                username: '',-->
-<!--                password: ''-->
-<!--            } as Form,-->
-<!--            rules: {-->
-<!--                username: [-->
-<!--                    { required: true, message: '请输入用户名', trigger: 'blur' }-->
-<!--                ],-->
-<!--                password: [-->
-<!--                    { required: true, message: '请输入密码', trigger: 'blur' }-->
-<!--                ]-->
-<!--            }-->
-<!--        };-->
-<!--    },-->
-<!--    methods: {-->
-<!--        handleLogin() {-->
-<!--            (this.$refs.loginForm as any).validate(valid => {-->
-<!--                if (valid) {-->
-<!--                    // 发送登录请求-->
-<!--                    console.log('登录成功');-->
-<!--                } else {-->
-<!--                    console.log('登录失败');-->
-<!--                    return false;-->
-<!--                }-->
-<!--            });-->
-<!--        }-->
-<!--    }-->
-<!--});-->
-<!--</script>-->
-
-<!--<style scoped>-->
-<!--.login-container {-->
-<!--    display: flex;-->
-<!--    justify-content: center;-->
-<!--    align-items: center;-->
-<!--    height: 100vh;-->
-<!--}-->
-
-<!--.login-card {-->
-<!--    width: 360px;-->
-<!--}-->
-
-<!--.login-form {-->
-<!--    padding: 20px;-->
-<!--}-->
-
-<!--.login-title {-->
-<!--    text-align: center;-->
-<!--    margin-bottom: 20px;-->
-<!--}-->
-<!--</style>-->
+.login-title {
+    text-align: center;
+    margin-bottom: 20px;
+}
+</style>
