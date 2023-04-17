652d9875dba5fe5991cc7722fd2c32da3784020e
Xander5522 <71019281+Xander5522@users.noreply.github.com>
2023-04-17 22:41:02 +0800

    update AdminLogin.vue

--- webapp/src/views/AdminLogin.vue
+++ webapp/src/views/AdminLogin.vue
@@ -1,49 +1,89 @@
 <template>
     <div class="login-container">
-        <el-form ref="loginForm" :model="loginForm" label-width="80px">
-            <h1 class="login-title">登录</h1>
-            <el-form-item label="用户名" prop="username">
-                <el-input v-model="loginForm.username" placeholder="请输入用户名"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-                <el-input v-model="loginForm.password" type="password" placeholder="请输入密码"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="handleLogin">登录</el-button>
-            </el-form-item>
-        </el-form>
+        <el-card class="login-card">
+            <div class="login-form">
+                <h2 class="login-title">用户登录</h2>
+                <el-form ref="loginForm" :model="form" :rules="rules" label-width="0px">
+                    <el-form-item prop="username">
+                        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item prop="password">
+                        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="handleLogin">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </el-card>
     </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-import { ElForm, ElFormItem, ElInput, ElButton } from 'element-plus';
+import { defineComponent } from 'vue';
+import { ElForm, ElFormItem, ElInput, ElButton, ElCard } from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
-@Component({
+interface Form {
+    username: string;
+    password: string;
+}
+
+export default defineComponent({
+    name: 'Login',
     components: {
         ElForm,
         ElFormItem,
         ElInput,
-        ElButton
+        ElButton,
+        ElCard
+    },
+    data() {
+        return {
+            form: {
+                username: '',
+                password: ''
+            } as Form,
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' }
+                ]
+            }
+        };
+    },
+    methods: {
+        handleLogin() {
+            (this.$refs.loginForm as any).validate(valid => {
+                if (valid) {
+                    // 发送登录请求
+                    console.log('登录成功');
+                } else {
+                    console.log('登录失败');
+                    return false;
+                }
+            });
+        }
     }
-})
-export default class LoginPage extends Vue {
-    loginForm = {
-        username: '',
-        password: ''
-    };
-
-    handleLogin() {
-        // 登录逻辑
-        console.log('登录成功');
-    }
-}
+});
 </script>
 
 <style scoped>
 .login-container {
-    width: 300px;
-    margin: 100px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.login-card {
+    width: 360px;
+}
+
+.login-form {
+    padding: 20px;
 }
 
 .login-title {
